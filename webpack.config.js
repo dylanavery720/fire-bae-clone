9f6ac49218249d1cdc4e3f51a79915b0de916683
rrgayhart <rrgayhart@gmail.com>
2016-12-14 09:50:57 -0700

    Set up initial build

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: "./lib/index.js",
+    test: "mocha!./test/index.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/,
+        exclude: '/node_modules|bower_components/',
+        loader: 'babel-loader',
+        query: { presets: ['es2015', 'react'] } },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json', '.scss', '.css']
+  }
+}
