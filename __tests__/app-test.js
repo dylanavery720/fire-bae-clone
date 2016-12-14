e7a6357545e176305f47f779505fad5b2d2402fb
rrgayhart <rrgayhart@gmail.com>
2016-12-14 12:19:59 -0700

    Add sign in and sign out prompts

--- __tests__/app-test.js
+++ __tests__/app-test.js
@@ -9,3 +9,21 @@ it('renders with a title', () => {
 
   expect(app.contains('New Title')).toEqual(true);
 });
+
+it('gives you a signIn prompt when not logged in', () => {
+  const app = shallow(
+    <App />
+  );
+
+  expect(app.contains('Sign In')).toEqual(true);
+});
+
+it('gives you a signOut prompt when logged in', () => {
+  const app = shallow(
+    <App />
+  );
+
+  app.setState({user: []});
+  
+  expect(app.contains('Sign Out')).toEqual(true);
+});
